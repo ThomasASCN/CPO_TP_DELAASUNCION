446f2bf64809eb3175dde4ef28d871927189371d
ThomasASCN <thomas.de.la.asuncion.pro@gmail.com>
2023-09-22 15:29:57 +0200

    tp 2 exo 2 en cours

---
+++ TP2_convertisseurObjet_DELAASUNCION/src/tp2_convertisseurobjet_delaasuncion/TP2_convertisseurObjet_DELAASUNCION.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_delaasuncion;
+
+/**
+ *
+ * @author De la asuncion
+ */
+public class TP2_convertisseurObjet_DELAASUNCION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         convertisseur convertisseur1 = new convertisseur();
+    }
+    
+}
---
+++ TP2_convertisseurObjet_DELAASUNCION/src/tp2_convertisseurobjet_delaasuncion/convertisseur.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_delaasuncion;
+
+/**
+ *
+ * @author De la asuncion
+ */
+public class convertisseur {
+    int nbConversions;
+    
+public convertisseur () {
+nbConversions = 0 ;
+}   
+@Override
+public String toString () {
+return "nb de conversions"+ nbConversions;
+}
+}
