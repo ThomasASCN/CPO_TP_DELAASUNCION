392ba064efbfce253a438798cbc5b62948772700
ThomasASCN <thomas.de.la.asuncion.pro@gmail.com>
2023-10-16 17:02:00 +0200

    tp 2 exo 3

---
+++ TP2_manip_DELAASUNCION/src/tp2_manip_delaasuncion/Moussaka.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_delaasuncion;
+
+/**
+ *
+ * @author De la asuncion
+ */
+public class Moussaka {
+    int nbCalories;
+    
+    
+public Moussaka(int lenbCalories){
+        nbCalories = lenbCalories;
+}
+}
